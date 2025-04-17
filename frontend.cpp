f0fa1076aebe0a8fc2003f6dddb6126ee06e16ee
Ryan Mijares <166569891+Mijares-ryan@users.noreply.github.com>
2025-04-16 22:06:45 -0400

    Update frontend.cpp

--- frontend.cpp
+++ frontend.cpp
@@ -10,10 +10,6 @@
 
 using namespace std;
 
-bool compareByReviewScore(const Game &a, const Game &b) {
-    return a.review_score > b.review_score;
-}
-
 void runFrontend() {
     const int WINDOW_WIDTH = 1200;
     const int WINDOW_HEIGHT = 800;
