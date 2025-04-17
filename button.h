06853408544b589bad58c3b93ae048805e81b30d
Ryan Mijares <166569891+Mijares-ryan@users.noreply.github.com>
2025-04-16 22:04:48 -0400

    Update button.h

--- button.h
+++ button.h
@@ -1,1 +1,22 @@
+#pragma once
 
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+class Button {
+private:
+    sf::CircleShape outerCircle;
+    sf::CircleShape innerCircle;
+    sf::Text typeOfSort;
+    sf::FloatRect clickingArea;
+    bool isSelected;
+public:
+    Button();
+    Button(float x, float y, const string& text, sf::Font& font);
+    void draw(sf::RenderWindow& window);
+    bool checkIsSelected(sf::Vector2f mousePosition);
+    void deselect();
+    bool getSelected();
+    string getTypeOfSort();
+};
